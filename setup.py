7e3b2f691f23bcf6afa787b45d0d9ccbf160dc45
Hanno Schlichting <hanno@hannosch.eu>
2010-07-16 14:16:40 +0000

    Update license to GPL version 2 only.
    svn path=/plone.portlet.static/trunk/; revision=37902

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = '2.0b3'
+version = '2.0'
 
 setup(name='plone.portlet.static',
       version=version,
@@ -8,14 +8,18 @@ setup(name='plone.portlet.static',
       long_description=open("README.txt").read() + "\n" +
                        open("CHANGES.txt").read(),
       classifiers=[
-        "Framework :: Plone",
-        "Programming Language :: Python",
+          "Environment :: Web Environment",
+          "Framework :: Plone",
+          "Framework :: Zope2",
+          "License :: OSI Approved :: GNU General Public License (GPL)",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
         ],
-      keywords='plone portlet',
-      author='Martin Aspeli',
+      keywords='plone portlet static',
+      author='Plone Foundation',
       author_email='plone-developers@lists.sourceforge.net',
-      url='http://plone.org',
-      license='GPL',
+      url='http://pypi.python.org/pypi/plone.portlet.static',
+      license='GPL version 2',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=["plone", 'plone.portlet'],
       include_package_data=True,
