0818679b7f7eb5258b1a1e357524f888187ee25d
David Glick <davidglick@groundwire.org>
2010-11-27 05:00:06 +0000

    add Site Administrator role to the portlet add permission
    svn path=/plone.portlet.static/trunk/; revision=45981

--- plone/portlet/static/__init__.py
+++ plone/portlet/static/__init__.py
@@ -2,4 +2,5 @@ from zope.i18nmessageid import MessageFactory
 PloneMessageFactory = MessageFactory('plone')
 
 from Products.CMFCore.permissions import setDefaultRoles
-setDefaultRoles('plone.portlet.static: Add static portlet', ('Manager', 'Owner',))
+setDefaultRoles('plone.portlet.static: Add static portlet',
+                ('Manager', 'Site Administrator', 'Owner',))
