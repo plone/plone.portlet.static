0b684903e32fea63d4520c1ab546b6260cc97cd1
Maurits van Rees <maurits@vanrees.org>
2022-11-23 00:27:44 +0100

    classifiers
    [ci skip]

--- setup.py
+++ setup.py
@@ -33,9 +33,10 @@ setup(
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
     keywords='plone portlet static',
     author='Plone Foundation',
@@ -62,7 +63,7 @@ setup(
         'zope.i18nmessageid',
         'zope.interface',
         'zope.schema',
-        'Zope2',
+        'Zope',
     ],
     entry_points="""
     [z3c.autoinclude.plugin]
